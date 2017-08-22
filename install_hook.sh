0ddcb99dcafce98ca904f1c2774a7ec59485b1af
Lock <zzlzhang@cn.ibm.com>
2017-08-22 19:56:34 +0800

    fix bp

--- install_hook.sh
+++ install_hook.sh
@@ -744,7 +744,7 @@ install_bp()
     __logging "${FUNCNAME[0]}" "$LINENO" "INFO" "sc4bp_script_path: ${sc4bp_script_path}"
 
     [[ -d "${bp_instance_web}" ]] && rm -Rf "${bp_instance_web}"
-    cp -rp "${bp_instance_web}" "${bp_instance_web}"
+    cp -rp "${WEB_DIR}/${INSTANCE_NAME}" "${bp_instance_web}"
 
     # 修正 .htaccess 文件
     __command_logging_and_exit "${FUNCNAME[0]}" "$LINENO" "sed 's^/${INSTANCE_NAME}^/${bp_instance_name}^g' ${WEB_DIR}/${INSTANCE_NAME}/.htaccess > ${bp_instance_web}/.htaccess"
